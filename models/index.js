f13e0ccdd5e3e0afbd75efc39dcab5339349cc6e
joydotcom <msjoyjackson@gmail.com>
2021-04-26 08:34:06 -0400

    added to tag and product tag models

--- models/index.js
+++ models/index.js
@@ -15,9 +15,15 @@ Category.hasMany(Product, {
   foreignKey: 'category_id',
 });
 // Categories have many Products
-
+Product.belongsToMany(Tag, {
+  through: ProductTag,
+  foreignKey: 'product_id',
+});
 // Products belongToMany Tags (through ProductTag)
-
+Tag.belongsToMany(Product, {
+  through: ProductTag,
+  foreignKey: 'tag_id',
+});
 // Tags belongToMany Products (through ProductTag)
 
 module.exports = {
